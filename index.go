d4c8c8b2dacc138d210f11095aaaab33d0a6aeda
Ahmed Abdelhak <engineer.ahmedabdelhak@gmail.com>
2020-03-23 18:55:50 +0200

    - refactored main()

--- index.go
+++ index.go
@@ -9,34 +9,28 @@ import (
 	"github.com/go-redis/redis"
 )
 
-var client *redis.Client
-var result string
-
 func main() {
+	ageOfEmpiresGame()
+}
 
-	// disclaimer: I don't like writing comments in code, because it supposed to be readable
-	// but It's my first time to write code in Go, that's why I keep commenting as a logger 													for what I do
+func ageOfEmpiresGame() {
+	var client *redis.Client
+	var result string
 
-	// reading input from user
-	var name string = getUnitName()
+	name := getUnitName()
 
-	// Configurations of Redis
-	client = redisConfig()
+	client = redisInitialize()
 
-	// get from Cache
 	result, err := client.Get(name).Result()
-	check(err)
+	Logger(err)
 
-	// HTTP Call to endpoint if cache is empty
 	if len(result) == 0 {
 		result = getUnitByNameOrId(name)
 	}
 
-	// saving to Cache
 	err = client.Set(name, result, 0).Err() // cache timeout is zero
-	check(err)
+	Logger(err)
 
-	// logging the output to the user
 	log.Println("Unit Details are below: ")
 	log.Println(result)
 }
@@ -47,16 +41,16 @@ func getUnitName() string {
 	fmt.Println("Please enter the Unit name")
 	_, err := fmt.Scan(&name)
 
-	check(err)
+	Logger(err)
 
 	return name
 }
 
-func redisConfig() (client *redis.Client) {
+func redisInitialize() (client *redis.Client) {
 	return redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     "localhost:6379", // make sure that your Redis-Server is running
+		Password: "",               // no password set
+		DB:       0,                // use default DB
 	})
 }
 
@@ -64,18 +58,18 @@ func getUnitByNameOrId(name string) string {
 
 	resp, err := http.Get(fmt.Sprintf("https://age-of-empires-2-api.herokuapp.com/api/v1/unit/%v", name))
 
-	check(err)
+	Logger(err)
 
 	body, err := ioutil.ReadAll(resp.Body)
 
-	check(err)
+	Logger(err)
 
 	defer resp.Body.Close()
 
 	return string(body)
 }
 
-func check(err error) {
+func Logger(err error) {
 	if err != nil {
 		fmt.Println(err)
 		return
